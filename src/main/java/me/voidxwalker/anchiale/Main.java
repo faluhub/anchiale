dafec5ca286028c5865e2f426317f6122ed45364
Wurgo <71964154+wurgo@users.noreply.github.com>
2022-01-26 08:50:35 +0100

    Update Main.java

--- src/main/java/me/voidxwalker/anchiale/Main.java
+++ src/main/java/me/voidxwalker/anchiale/Main.java
@@ -8,12 +8,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main implements ModInitializer {
-    public static Logger LOGGER = LogManager.getLogger();
     private static final ModContainer mod = FabricLoader.getInstance().getModContainer("anchiale").orElseThrow(NullPointerException::new);
     public static String MOD_NAME = mod.getMetadata().getName();
-    public static void log(Level level, String message) {
-        LOGGER.log(level, "[" + MOD_NAME + "] " + message);
-    }
+    public static Logger LOGGER = LogManager.getLogger();
+    
+    public static void log(Level level, String message) { LOGGER.log(level, "[" + MOD_NAME + "] " + message); }
+    
     @Override
     public void onInitialize() {
         log(Level.INFO, "Initializing");
